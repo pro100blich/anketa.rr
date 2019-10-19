8e91cc20e36f36aba0eb442c0393a079c2f14492
Alexey <pro100blich@gmail.com>
2019-10-19 16:38:59 +1000

    git 4

--- index.php
+++ index.php
@@ -5,20 +5,20 @@ header("Content-Type:text/html;charset=UTF-8");
 require_once("config.php");
 require_once("classes/ACore.php");
 require_once("classes/ACore_Admin.php");
-if (isset($_SESSION['sess'])) 
+if (isset($_SESSION['sess']))
 {
 	if($_GET['option']) {
 	$class = trim(strip_tags($_GET['option']));
 }
 else {
-	$class = 'main';	
+	$class = 'main';
 }
 
 if(file_exists("classes/".$class.".php")) {
 	///
 	include("classes/".$class.".php");
 	if(class_exists($class)) {
-		
+
 		$obj = new $class;
 		$obj->get_body();
 	}
@@ -29,7 +29,7 @@ if(file_exists("classes/".$class.".php")) {
 else {
 	exit("<p>Не правильный адресс</p>");
 }
-	//echo '<script type="text/javascript"> window.location = "../classes/admin.php"</script>';
+	//echo '  <script type="text/javascript"> window.location = "../classes/admin.php"</script>';
 }
 else{
 	echo '<script type="text/javascript"> window.location = "../login.php"</script>';
